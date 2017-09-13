f4a0aced59fff21d3fe39eae7f75af72e024f121
Jimmy Ramos <jimmy.ramos@unitec.edu>
2017-09-12 22:40:36 -0600

    refactor init

--- Rigio/Rigio/Views/LoginPage.cs
+++ Rigio/Rigio/Views/LoginPage.cs
@@ -11,11 +11,16 @@ namespace Rigio.Views
 {
     public class LoginPage : ContentPage
     {
-        readonly Label _hintLabel;
+        Label _hintLabel;
         readonly List<Button> _loginButtons = new List<Button>();
         bool _isAuthenticated;
 
         public LoginPage()
+        {
+            InitUi();
+        }
+
+        private void InitUi()
         {
             Title = "Rigio Login";
 
@@ -28,118 +33,128 @@ namespace Rigio.Views
             var stackLayout = new StackLayout
             {
                 VerticalOptions = LayoutOptions.Center,
-                Children = { _hintLabel }
+                Children = {_hintLabel}
             };
 
-            var providers = new[] { "Facebook"};
+            var providers = new[] {"Facebook"};
             foreach (var provider in providers)
             {
-                var loginButton = new Button
-                {
-                    HorizontalOptions = LayoutOptions.Center,
-                    Text = $"Login {provider}",
-                    AutomationId = provider,
-                    HeightRequest = 35
-                };
-
-                loginButton.Clicked += LoginButtonOnClicked;
-
-                _loginButtons.Add(loginButton);
-                stackLayout.Children.Add(loginButton);
+                InitLoginButton(provider, stackLayout);
             }
 
             Content = stackLayout;
+        }
+
+        private void InitLoginButton(string provider, StackLayout stackLayout)
+        {
+            var loginButton = new Button
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                Text = $"Login {provider}",
+                AutomationId = provider,
+                HeightRequest = 35
+            };
+
+            loginButton.Clicked += LoginButtonOnClicked;
+
+            _loginButtons.Add(loginButton);
+            stackLayout.Children.Add(loginButton);
         }
 
         async void LoginButtonOnClicked(object sender, EventArgs e)
         {
             if (_isAuthenticated)
             {
-                _hintLabel.Text = "Unauthenticated";
+                RedirectToMainPage(sender);
+            }
+            else
+            {
+                await Login(sender);
+            }
+        }
 
-                var senderBtn = sender as Button;
-                if (senderBtn == null) return;
+        private void RedirectToMainPage(object sender)
+        {
+            _hintLabel.Text = "Unauthenticated";
 
-                Logout(senderBtn.AutomationId);
+            var senderBtn = sender as Button;
+            if (senderBtn == null) return;
 
-                _isAuthenticated = false;
-                foreach (var btn in _loginButtons)
-                {
-                    btn.IsEnabled = true;
-                    btn.Text = $"Login {btn.AutomationId}";
-                }
-                
+            //Logout(senderBtn.AutomationId);
+
+            _isAuthenticated = false;
+            foreach (var btn in _loginButtons)
+            {
+                btn.IsEnabled = true;
+                btn.Text = $"Login {btn.AutomationId}";
+            }
+
+            Application.Current.MainPage = new NavigationPage(new MainPage());
+        }
+
+        private async Task Login(object sender)
+        {
+            var senderBtn = sender as Button;
+            if (senderBtn == null) return;
+
+            _hintLabel.Text = "Login. Please wait";
+            var loginResult = await LoginToProvider(senderBtn.AutomationId);
+        
+            foreach (var btn in _loginButtons.Where(b => b != senderBtn))
+                btn.IsEnabled = false;
+
+            await ProccessLoginResult(loginResult, senderBtn);
+        }
+
+        private async Task ProccessLoginResult(LoginResult loginResult, Button senderBtn)
+        {
+            switch (loginResult.LoginState)
+            {
+                case LoginState.Canceled:
+                    _hintLabel.Text = "Canceled";
+                    foreach (var btn in _loginButtons.Where(b => b != senderBtn))
+                        btn.IsEnabled = true;
+                    break;
+                case LoginState.Success:
+                    var account = await App.AccountManager.GetAccountAsync(loginResult.Token);
+
+                    ValidateAccount(account, senderBtn);
+                    break;
+                default:
+                    _hintLabel.Text = "Failed: " + loginResult.ErrorString;
+                    foreach (var btn in _loginButtons.Where(b => b != senderBtn))
+                        btn.IsEnabled = true;
+                    break;
+            }
+        }
+
+        private void ValidateAccount(Account account, Button senderBtn)
+        {
+            if (account != null)
+            {
+                App.Account.Access_Token = account.Access_Token;
+                App.Account.UserId = account.UserId;
+                _isAuthenticated = true;
                 Application.Current.MainPage = new NavigationPage(new MainPage());
             }
             else
             {
-                var senderBtn = sender as Button;
-                if (senderBtn == null) return;
-                
-                _hintLabel.Text = "Login. Please wait";
-                var loginResult = await Login(senderBtn.AutomationId);
-
-
+                _hintLabel.Text = "Failed try again";
                 foreach (var btn in _loginButtons.Where(b => b != senderBtn))
-                    btn.IsEnabled = false;
-
-                switch (loginResult.LoginState)
-                {
-                    case LoginState.Canceled:
-                        _hintLabel.Text = "Canceled";
-                        foreach (var btn in _loginButtons.Where(b => b != senderBtn))
-                            btn.IsEnabled = true;
-                        break;
-                    case LoginState.Success:
-                        var account = await App.AccountManager.GetAccountAsync(loginResult.Token);
-
-                        if (account != null)
-                        {
-                            App.Account.Access_Token = account.Access_Token;
-                            App.Account.UserId = account.UserId;
-
-                            _isAuthenticated = true;
-
-                            Application.Current.MainPage = new NavigationPage(new MainPage());
-                        }
-                        else
-                        {
-                            _hintLabel.Text = "Failed try again";
-                            foreach (var btn in _loginButtons.Where(b => b != senderBtn))
-                                btn.IsEnabled = true;
-                        }
-
-                        break;
-                    default:
-                        _hintLabel.Text = "Failed: " + loginResult.ErrorString;
-                        foreach(var btn in _loginButtons.Where(b => b != senderBtn))
-                            btn.IsEnabled = true;
-                        break;
-                }
+                    btn.IsEnabled = true;
             }
         }
-
-        Task<LoginResult> Login(string providerName)
+        
+        Task<LoginResult> LoginToProvider(string providerName)
         {
+
+            // get rid of switch
             switch (providerName.ToLower())
             {
                 case "facebook":
                     return DependencyService.Get<IFacebookService>().Login();
                 default:
                     return DependencyService.Get<IFacebookService>().Login();
-            }
-        }
-
-        void Logout(string providerName)
-        {
-            switch (providerName.ToLower())
-            {
-                case "facebook":
-                    DependencyService.Get<IFacebookService>().Logout();
-                    return;
-                default:
-                    DependencyService.Get<IFacebookService>().Logout();
-                    return;
             }
         }
     }
--- Rigio/Rigio/Views/Menu/RigioPage.xaml.cs
+++ Rigio/Rigio/Views/Menu/RigioPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Rigio.Models;
 using Rigio.Renderers;
 using Rigio.Views.Rigios;
@@ -19,11 +20,16 @@ namespace Rigio.Views.Menu
 
             InitializeComponent();
 
+            InitializeUi();
+        }
+
+        private void InitializeUi()
+        {
+            ScrollView _ScrollView;
             lvRigios.ItemTemplate = new DataTemplate(typeof(RigioCell));
-           
+
             _ScrollView = new ScrollView
             {
-                
                 Content = new StackLayout
                 {
                     Spacing = 0,
@@ -34,59 +40,68 @@ namespace Rigio.Views.Menu
                 }
             };
 
-            #region compose view hierarchy
             if (Device.RuntimePlatform.Equals("Android"))
             {
-                _Fab = new FloatingActionButtonView
-                {
-                    ImageName = "fab_add.png",
-                    ColorNormal = Color.FromHex("53BA9D"),
-                    ColorPressed = Color.FromHex("42947D"),
-                    ColorRipple = Color.FromHex("53BA9D"),
-
-                    Clicked = NavigateToCreateMatch
-                };
-
-                var absolute = new AbsoluteLayout
-                {
-                    VerticalOptions = LayoutOptions.FillAndExpand,
-                    HorizontalOptions = LayoutOptions.FillAndExpand,
-                };
-
-                // Position the pageLayout to fill the entire screen.
-                // Manage positioning of child elements on the page by editing the pageLayout.
-                AbsoluteLayout.SetLayoutFlags(_ScrollView, AbsoluteLayoutFlags.All);
-                AbsoluteLayout.SetLayoutBounds(_ScrollView, new Rectangle(0f, 0f, 1f, 1f));
-                absolute.Children.Add(_ScrollView);
-
-                // Overlay the FAB in the bottom-right corner
-                AbsoluteLayout.SetLayoutFlags(_Fab, AbsoluteLayoutFlags.PositionProportional);
-                AbsoluteLayout.SetLayoutBounds(_Fab, new Rectangle(1f, 1f, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
-                absolute.Children.Add(_Fab);
-
-                Content = absolute;
+                SetupAndroidAddButton(_ScrollView);
             }
             else
             {
-                var addButton = new ToolbarItem
-                {
-                    Text = "Add",
-                    Icon = "add_ios_gray",
-                    //Order = ToolbarItemOrder.Primary
-                };
-
-                addButton.Clicked += async (sender, args) =>
-                {
-                    NavigationPage.SetBackButtonTitle(this, "Back");
-
-                    await Navigation.PushAsync(new CreateRigioView(new Match(), false));
-                };
-
-                ToolbarItems.Add(addButton);
-
-                Content = _ScrollView;
+                SetupIOsAddButton(_ScrollView);
             }
-            #endregion
+        }
+
+        private void SetupIOsAddButton(ScrollView _ScrollView)
+        {
+            var addButton = new ToolbarItem
+            {
+                Text = "Add",
+                Icon = "add_ios_gray",
+                //Order = ToolbarItemOrder.Primary
+            };
+
+            addButton.Clicked += async (sender, args) =>
+            {
+                NavigationPage.SetBackButtonTitle(this, "Back");
+
+                await Navigation.PushAsync(new CreateRigioView(new Match(), false));
+            };
+
+            ToolbarItems.Add(addButton);
+
+            Content = _ScrollView;
+        }
+
+        private void SetupAndroidAddButton(ScrollView _ScrollView)
+        {
+            FloatingActionButtonView _Fab;
+            _Fab = new FloatingActionButtonView
+            {
+                ImageName = "fab_add.png",
+                ColorNormal = Color.FromHex("53BA9D"),
+                ColorPressed = Color.FromHex("42947D"),
+                ColorRipple = Color.FromHex("53BA9D"),
+
+                Clicked = NavigateToCreateMatch
+            };
+
+            var absolute = new AbsoluteLayout
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+
+            // Position the pageLayout to fill the entire screen.
+            // Manage positioning of child elements on the page by editing the pageLayout.
+            AbsoluteLayout.SetLayoutFlags(_ScrollView, AbsoluteLayoutFlags.All);
+            AbsoluteLayout.SetLayoutBounds(_ScrollView, new Rectangle(0f, 0f, 1f, 1f));
+            absolute.Children.Add(_ScrollView);
+
+            // Overlay the FAB in the bottom-right corner
+            AbsoluteLayout.SetLayoutFlags(_Fab, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(_Fab, new Rectangle(1f, 1f, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+            absolute.Children.Add(_Fab);
+
+            Content = absolute;
         }
 
         Action<object, EventArgs> NavigateToCreateMatch
